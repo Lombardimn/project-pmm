daf6348dd917a2fb8de7e58baf674f651f4ca4e8
Lombardimn <matiiaslombardi@gmail.com>
2024-06-03 17:58:27 -0300

    client v2

--- server/services/client/controllers/client.controllers.js
+++ server/services/client/controllers/client.controllers.js
@@ -1,9 +1,69 @@
-export const getClients = async (req, res) => {}
+export const getClients = async (req, res) => {
+  try {
+    const [result] = await pool.query(
+      `
+        SELECT *
+        FROM Clients
+        JOIN Client_relation
+        ON Clients.id = Client_relation.id
+        JOIN Directions
+        ON Client_relation.Direction_id = Directions.id
+        ORDER BY Client.id ASC
+      `
+    )
 
-export const getClient = async (req, res) => {}
+    res.json(result)
 
-export const createClient = async (req, res) => {}
+  } catch (error) {
+    return res.status(500)
+      .json({
+        message: error.message
+      })
+  }
+}
 
-export const updateClient = async (req, res) => {}
+export const getClient = async (req, res) => {
+  try {
+    const [result] = await pool.query(
+      'SELECT * FROM Clients WHERE id = ?',
+      [req.params.client]
+    )
 
-export const deleteClient = async (req, res) => {}
+    res.json(result)
+
+  } catch (error) {
+    return res.status(500)
+      .json({
+        message: error.message
+      })
+  }
+}
+
+export const createClient = async (req, res) => {
+  const { 
+    name,
+    last_name,
+    area_phone,
+    number_phone,
+    street,
+    number_street,
+    postal_code,
+    floor,
+    apartment,
+    block,
+    neighborhood,
+    city,
+    locality,
+    state,
+  } = req.body
+
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+export const updateClient = async (req, res) => { }
+
+export const deleteClient = async (req, res) => { }
---
+++ server/services/client/controllers/direction.controllers.js
@@ -0,0 +1,9 @@
+export const getDirections = async (req, res) => {}
+
+export const getDirection = async (req, res) => {}
+
+export const createDitrection = async (req, res) => {}
+
+export const updateDirection = async (req, res) => {}
+
+export const deleteDirection = async (req, res) => {}
