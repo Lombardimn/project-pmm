0a6f71a7441c92ba4cc50a9b3e0f2d7a68442944
Lombardimn <matiiaslombardi@gmail.com>
2024-06-06 14:03:46 -0300

    product v2

--- server/services/product/controllers/product.controllers.js
+++ server/services/product/controllers/product.controllers.js
@@ -1,9 +1,42 @@
+import { pool } from '../models/product.model.js'
+
 export const getProducts = async (req, res) => {
-  res.json('Products')
+  try {
+  const [result] = await pool.query(
+    'SELECT * FROM Products ORDER BY id ASC'
+  )
+
+  res.json(result)
+
+  } catch (error) {
+    return res.status(500)
+      .json({
+        message: error.message
+      })    
+  }
 }
 
 export const getProduct = async (req, res) => {
-  res.json('Product ' + req.params.id)
+  const { name } = req.body
+
+  try {
+    const [result] = pool.query(
+    'SELECT * FROM Products WHERE Products.id = ? AND Products.name = ?',
+      [
+        req.params.id,
+        name
+      ]
+    )
+  
+    console.log(result)
+    res.json(result + req.params.id)
+
+  } catch (error) {
+    return res.status(500)
+      .json({
+        message: error.message
+      })
+  }
 }
 
 export const createProduct = async (req, res) => {}
