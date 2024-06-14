e5cdf24c3b801b11d911d66e9fad5946f2387379
Lombardimn <matiiaslombardi@gmail.com>
2024-06-14 17:18:35 -0300

    change upload_reset

--- client/src/pages/Private/Profile/services/uploadToCloud.service.ts
+++ client/src/pages/Private/Profile/services/uploadToCloud.service.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const CLOUDINARY_URL = import.meta.env.VITE_CLOUD_API_URL
+const CLOUD_UPLOAD_PRESET = import.meta.env.VITE_CLOUD_UPLOAD_PRESET
 
 export interface CloudinaryUploadResult {
   secure_url: string
@@ -9,7 +10,7 @@ export interface CloudinaryUploadResult {
 export const uploadToCloudinary = async (file: File): Promise<CloudinaryUploadResult> => {
   const formData = new FormData()
   formData.append('file', file)
-  formData.append('upload_preset', 'awowomfx')
+  formData.append('upload_preset', CLOUD_UPLOAD_PRESET)
 
   try {
     const response = await axios.post(CLOUDINARY_URL, formData, {
